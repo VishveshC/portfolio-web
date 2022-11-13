6420a29b736a181ead616cba5ac917ba3836da71
Vishvesh <stepper1947@gmail.com>
2022-11-13 16:24:11 +0530

    added temp

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 function preloadFunc() {
+    let ethericon = document.getElementById('eth');
+    let wifiicon = document.getElementById('icons-wifi');
+
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    alert("OI using mob");
-    window.location.href = "mob.html";
+        alert("Using a mobile. Please use a PC to visit this website.");
+        /*window.location.href = "mob.html";*/
+        let ethericon = document.getElementById('eth');
+        let wifiicon = document.getElementById('icons-wifi');
+        ethericon.style.display = "none";
+        wifiicon.style.display = "block";
+    }
+    else {
+        let ethericon = document.getElementById('eth');
+        let wifiicon = document.getElementById('icons-wifi');
+        ethericon.style.display = "block";
+        wifiicon.style.display = "none";
     }
 }
-window.onpaint = preloadFunc(); 
+window.onload = preloadFunc(); 
